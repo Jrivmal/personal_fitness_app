bfea31faa3b959e427abf71f511393e87a5c3841
Jrivmal <j_rm78@yahoo.com>
2024-06-19 11:38:06 -0400

    white label error page

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -18,7 +18,7 @@ import org.springframework.context.annotation.Bean;
  *
  */
 @SpringBootApplication
-@Theme(value = "my-app")
+@Theme(value = "personal_fitness_app4")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
